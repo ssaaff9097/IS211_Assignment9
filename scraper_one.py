eaa9e9918bdb7ba7a1c8355e868acf548fd98b46
Samantha Fontanez <samantha.fontanez20@spsmail.cuny.edu>
2024-10-27 18:54:40 -0400

    Week 9

--- scraper_one.py
+++ scraper_one.py
@@ -1,2 +1,27 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://www.cbssports.com/nfl/stats/"
+
+def get_football_stats():
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    table = soup.find('table', {'class': 'TableBase-table'})
+    players = []
+    for row in table.find_all('tr')[1:21]:
+        columns = row.find_all('td')
+        if columns: 
+            player = {
+                'name': columns[0].text.strip(),
+                'position': columns[1].text.strip(),
+                'team': columns[2].text.strip(),
+                'touchdowns': columns[3].text.strip()
+            }
+            players.append(player)
+
+        for player in players:
+            print(f"Player: {player['name']}, Position: {player['position']}, Team: {player['team']}, Touchdowns: {player['touchdowns']}")
+
+
 if __name__ == "__main__":
-    pass
+    get_football_stats() 
--- scraper_two.py
+++ scraper_two.py
@@ -1,2 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://en.wikipedia.org/wiki/List_of_Super_Bowl_champions"
+
+def get_super_bowl_champions():
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    table = soup.find('table', {'class': 'wikitable'})
+
+
+    champions_data = [] 
+    for row in table.find_all('tr')[1:]:
+        columns = row.find_all('td')
+        print([col.text.strip() for col in columns])
+        if len(columns) >= 3:
+            year = columns[0].text.strip()
+            champion = columns[1].text.strip()
+            runner_up = columns[2].text.strip()
+            champions_data.append((year, champion, runner_up))
+
+    if champions_data: 
+        for year, champion, runner_up in champions_data:
+            print(f"Year: {year}, Champion: {champion}, Runner-up: {runner_up}")
+    else: 
+        print("No data found")
+
 if __name__ == "__main__":
-    pass
+    get_super_bowl_champions()
